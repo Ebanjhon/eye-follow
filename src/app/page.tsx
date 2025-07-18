379626b36bc0a5c1a96c39a6da53ea5266525b79
Ebanjhon <yjhoneban1403@gmail.com>
2025-07-18 15:26:28 +0000

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,72 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const boxRef = useRef<HTMLDivElement>(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 }); // vị trí x và y của chuột
+  const centerRef = useRef({ x: 0, y: 0 });// điểm nằm giữa của mắt
+  const [eyeDeg, setEyeDeg] = useState(0); // độ của măt
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const updateCenter = () => {
+      if (boxRef.current) {
+        const rect = boxRef.current.getBoundingClientRect();
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+        centerRef.current = { x: centerX, y: centerY };
+      }
+    };
+
+    updateCenter();
+    window.addEventListener("resize", updateCenter);
+    return () => window.removeEventListener("resize", updateCenter);
+  }, []);
+
+  useEffect(() => {
+    let animationFrameId: number;
+    const handleMouseMove = (event: MouseEvent) => {
+      animationFrameId = requestAnimationFrame(() => {
+        let dx = event.clientX - centerRef.current.x;
+        let dy = event.clientY - centerRef.current.y;
+        let radian = Math.atan2(dy, dx);
+        let rot = (radian * 180) / Math.PI + 90;
+        setEyeDeg(rot);
+      });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, []);
+
+  return (
+    <div className="w-full h-screen bg-[radial-gradient(at_center,_#0600AB,_#00033D)] flex justify-center items-center gap-3">
+      <div className="eye-ball w-50 h-50 rounded-full">
+        <div style={{ transform: `rotate(${eyeDeg}deg)` }}
+          ref={boxRef}
+          className="w-50 h-50 bg-amber-50 rounded-full items-center flex justify-center">
+          <div className="w-40 h-40  rounded-full flex justify-center">
+            <div style={{ transform: `rotate(${-eyeDeg}deg)` }}
+              className="w-15 h-15 bg-black rounded-full" >
+              <div className="w-5 h-5 bg-white rounded-full" />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className="eye-ball w-50 h-50 rounded-full">
+        <div style={{ transform: `rotate(${eyeDeg}deg)` }}
+          ref={boxRef}
+          className="w-50 h-50 bg-amber-50 rounded-full items-center flex justify-center">
+          <div className="w-40 h-40  rounded-full flex justify-center">
+            <div style={{ transform: `rotate(${-eyeDeg}deg)` }}
+              className="w-15 h-15 bg-black rounded-full" >
+              <div className="w-5 h-5 bg-white rounded-full" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
